1fce4dbc2ac462c0399a56d0b185591b38b1a46f
nicolevenachi <ludwing@unicauca.edu.co>
2025-07-30 11:55:34 -0500

    03_ColorPickerApp_03-AddingBootstrapAndBasicLayout

--- 03-color-picker-app/host/src/App.tsx
+++ 03-color-picker-app/host/src/App.tsx
@@ -1,11 +1,20 @@
 import ReactDOM from "react-dom/client";
 
+import "./index.css";
 import ColorPicker from 'colorPicker/ColorPicker'
 
 const App = () => (
   <>
-    <ColorPicker />
-
+    <h1 className="text-center bg-dark text-white p-2 h-2"> Color Picker </h1>
+    <div className="container mt-4">
+      <div className="row">
+        <div className="col-12 col-md-4">Lista de Colores</div>
+        <div className="col-12 col-md-8">
+          {/* defualt reoponse class - 12 (whole content) */}
+          <ColorPicker />
+        </div>
+      </div>
+    </div>
   </>
 );
 
--- 03-color-picker-app/mf-colorpicker/src/components/ColorPicker.tsx
+++ 03-color-picker-app/mf-colorpicker/src/components/ColorPicker.tsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
 
 const ColorPicker = () => {
   return (
-    <div>
-      adsasd
-    </div>
-  )
-}
+    <form>
+      <input
+        type="color"
+        className="form-control"
+        style={{
+          width: "100%",
+          height: "300px",
+        }}
+        title="Pick a color ..."
+      />
 
-export default ColorPicker
+      <div className="text-center">
+        <h2 className="mt-3 fw-bolder">
+          <div
+            style={{
+              width: "20px",
+              height: "20px",
+              display: "inline-block",
+              backgroundColor: "#2222",
+              margin: "0 1rem",
+              borderRadius: "100%",
+            }}
+          >
+          </div> #color </h2>
+      </div>
+    </form>
+  );
+};
+
+export default ColorPicker;
