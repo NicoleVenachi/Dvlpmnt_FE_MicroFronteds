d59ba6c088b13b922f42a439a69eaa6d06bb7817
nicolevenachi <ludwing@unicauca.edu.co>
2025-08-19 08:42:05 -0500

    03_ColorPickerApp_12-swalAlerts added

--- 03-color-picker-app/mf-colorlistt/src/components/ColorList.tsx
+++ 03-color-picker-app/mf-colorlistt/src/components/ColorList.tsx
@@ -1,8 +1,21 @@
+import Swal from "sweetalert2";
 
 const ColorList = ({ colorsList }: { colorsList: string[] }) => {
 
   // const colorsList = ['#000', '#60da94', '#54b2dd', '#d8d873', '#25254d', '#f0f', '#da2c7d'];
   console.log(colorsList);
+
+  const handleCopyColor = (color: string) => {
+    navigator.clipboard.writeText(color);
+    Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      title: `Color ${color} copied to clipboard!`,
+      showConfirmButton: false,
+      timer: 3500,
+      timerProgressBar: true,
+    })
+  }
 
   const fallBackErr = <div className="alert alert-danger" role="alert">
     <b> No pre-saved colors... </b>
@@ -23,6 +36,7 @@ const ColorList = ({ colorsList }: { colorsList: string[] }) => {
             borderRadius: '0.25rem',
             marginBottom: '0.5rem',
           }}
+          onClick={() => handleCopyColor(color)}
         >
           {color}
         </button>
