d22040c8f5b19749da6b88c45ff23a9c40c1c51a
nicolevenachi <ludwing@unicauca.edu.co>
2025-08-20 06:27:40 -0500

    03_ColorPickerApp_16-DevMode

--- 03-color-picker-app/mf-colorpicker/rspack.config.ts
+++ 03-color-picker-app/mf-colorpicker/rspack.config.ts
@@ -6,86 +6,92 @@ import { ModuleFederationPlugin } from "@module-federation/enhanced/rspack";
 
 import { mfConfig } from "./module-federation.config";
 
-const isDev = process.env.NODE_ENV === "development";
+// const isDev = process.env.NODE_ENV === "development";
 
 // Target browsers, see: https://github.com/browserslist/browserslist
 const targets = ["chrome >= 87", "edge >= 88", "firefox >= 78", "safari >= 14"];
 
-export default defineConfig({
-  context: __dirname,
-  entry: {
-    main: "./src/index.ts",
-  },
-  resolve: {
-    extensions: ["...", ".ts", ".tsx", ".jsx"],
-  },
+export default defineConfig((env, argv) => {
+  const isDev = argv.mode === "development";
 
-  devServer: {
-    port: 3001,
-    historyApiFallback: true,
-    watchFiles: [path.resolve(__dirname, "src")],
-  },
-  output: {
-    // You need to set a unique value that is not equal to other applications
-    uniqueName: "mf_colorpicker",
-    // publicPath must be configured if using manifest
-    publicPath: "https://colorpickermf.netlify.app/",
-  },
+  return {
+    context: __dirname,
+    entry: {
+      main: "./src/index.ts",
+    },
+    resolve: {
+      extensions: ["...", ".ts", ".tsx", ".jsx"],
+    },
 
-  experiments: {
-    css: true,
-  },
+    devServer: {
+      port: 3001,
+      historyApiFallback: true,
+      watchFiles: [path.resolve(__dirname, "src")],
+    },
+    output: {
+      // You need to set a unique value that is not equal to other applications
+      uniqueName: "mf_colorpicker",
+      // publicPath must be configured if using manifest
+      publicPath: isDev
+        ? "http://localhost:3001/"
+        : "https://colorpickermf.netlify.app/",
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.svg$/,
-        type: "asset",
-      },
-      {
-        test: /\.css$/,
-        use: ["postcss-loader"],
-        type: "css",
-      },
-      {
-        test: /\.(jsx?|tsx?)$/,
-        use: [
-          {
-            loader: "builtin:swc-loader",
-            options: {
-              jsc: {
-                parser: {
-                  syntax: "typescript",
-                  tsx: true,
-                },
-                transform: {
-                  react: {
-                    runtime: "automatic",
-                    development: isDev,
-                    refresh: isDev,
+    experiments: {
+      css: true,
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.svg$/,
+          type: "asset",
+        },
+        {
+          test: /\.css$/,
+          use: ["postcss-loader"],
+          type: "css",
+        },
+        {
+          test: /\.(jsx?|tsx?)$/,
+          use: [
+            {
+              loader: "builtin:swc-loader",
+              options: {
+                jsc: {
+                  parser: {
+                    syntax: "typescript",
+                    tsx: true,
+                  },
+                  transform: {
+                    react: {
+                      runtime: "automatic",
+                      development: isDev,
+                      refresh: isDev,
+                    },
                   },
                 },
+                env: { targets },
               },
-              env: { targets },
             },
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new rspack.HtmlRspackPlugin({
-      template: "./index.html",
-    }),
-    new ModuleFederationPlugin(mfConfig),
-    isDev ? new RefreshPlugin() : null,
-  ].filter(Boolean),
-  optimization: {
-    minimizer: [
-      new rspack.SwcJsMinimizerRspackPlugin(),
-      new rspack.LightningCssMinimizerRspackPlugin({
-        minimizerOptions: { targets },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new rspack.HtmlRspackPlugin({
+        template: "./index.html",
       }),
-    ],
-  },
+      new ModuleFederationPlugin(mfConfig),
+      isDev ? new RefreshPlugin() : null,
+    ].filter(Boolean),
+    optimization: {
+      minimizer: [
+        new rspack.SwcJsMinimizerRspackPlugin(),
+        new rspack.LightningCssMinimizerRspackPlugin({
+          minimizerOptions: { targets },
+        }),
+      ],
+    },
+  };
 });
