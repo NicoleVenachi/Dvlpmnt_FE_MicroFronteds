a510182f81e7872bc558d57a0754d5f538c01635
nicolevenachi <ludwing@unicauca.edu.co>
2025-09-04 10:11:29 -0500

    04_DbApp_09-REactErrorHandler

--- 04-db-app-2/host/src/pages/HomePage.tsx
+++ 04-db-app-2/host/src/pages/HomePage.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Counter from "counter/Counter";
+import ErrorHandler from '../components/ErrorHandler';
 
 const HomePage = () => {
   return (
     <div className='text-center'>
       <h1 className='font-bold text-xl'> Cunter MF</h1>
-      <Counter initialCounter={5} />
+
+      <ErrorHandler>
+        <Counter initialCounter={5} />
+        {/* <Counter /> */}
+      </ErrorHandler>
     </div>
   )
 }
