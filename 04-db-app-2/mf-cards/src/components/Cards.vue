d5aabeb4e3b558ea08a343a7e892f963881efcaf
nicolevenachi <ludwing@unicauca.edu.co>
2025-09-10 09:24:03 -0500

    04_DbApp_12-APIRequesrtFromVueComponent

--- 04-db-app-2/mf-cards/src/components/Cards.vue
+++ 04-db-app-2/mf-cards/src/components/Cards.vue
@@ -1,3 +1,40 @@
 <template>
-  <h1 class="text-center font-bold text-5xl">Vue Component NEW</h1>
+  <div>
+    <h1 class="text-center font-bold text-5xl">DB MF</h1>
+
+    <div
+      v-for="character in characters"
+      :key="character.id"
+      class="m-4 p-4 border rounded"
+    >
+      <h2 class="text-2xl font-semibold">
+        {{ character.name }} | {{ character.race }}
+      </h2>
+      <img
+        loading="lazy"
+        :src="character.image"
+        :alt="character.name"
+        width="200"
+        height="200"
+      />
+    </div>
+  </div>
 </template>
+
+<script setup>
+import { onMounted, ref } from "vue";
+
+const characters = ref([]);
+
+const apiBaseURL = "https://dragonball-api.com/api/characters";
+const getCharacters = async () => {
+  const response = await fetch(apiBaseURL);
+  const data = await response.json();
+  console.log(data);
+  characters.value = data.items;
+};
+
+onMounted(async () => {
+  await getCharacters();
+});
+</script>
