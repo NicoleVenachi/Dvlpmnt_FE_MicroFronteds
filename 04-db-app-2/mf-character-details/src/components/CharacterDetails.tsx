212f0f2b676c3c4cb1ef6e248427f6bf8a76cf8f
nicolevenachi <ludwing@unicauca.edu.co>
2025-09-11 10:35:45 -0500

    04_DbApp_16-FecthingCharacterDataPerPage

--- 04-db-app-2/mf-character-details/src/components/CharacterDetails.tsx
+++ 04-db-app-2/mf-character-details/src/components/CharacterDetails.tsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const CharacterDetails = () => {
+  const { id } = useParams();
+  const [characterData, setCharacterData] = useState(null as any)
+  const baseUrl = 'https://dragonball-api.com/api/characters';
+
+  const getData = async () => {
+    const res = await fetch(`${baseUrl}/${id}`);
+    const data = await res.json();
+    setCharacterData(data);
+  }
+  useEffect(() => {
+    getData();
+  }, [id])
+
+  console.log(characterData);
+
+
   return (
     <div>
-      Hola
+
+      {
+        characterData && (
+          <>
+            <div>
+              <img src={characterData.image} alt={characterData.name} />
+            </div>
+            <div>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Field</th>
+                    <th>Value</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th>Name</th>
+                    <td>{characterData.name}</td>
+                  </tr>
+                  <tr>
+                    <th>Origin Planet</th>
+                    <td>{characterData.originPlanet.name}</td>
+                  </tr>
+                  <tr>
+                    <th>Affiliation</th>
+                    <td>{characterData.affiliation}</td>
+                  </tr>
+                  <tr>
+                    <th>Race</th>
+                    <td>{characterData.race}</td>
+                  </tr>
+                  <tr>
+                    <th>Gender</th>
+                    <td>{characterData.gender}</td>
+                  </tr>
+                  <tr>
+                    <th>Transformations</th>
+                    <td>{characterData.race.length}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </>
+
+        )
+      }
+
     </div>
   )
 }
