8cbcaa6d419ff38321f14154653a57cf859c552f
nicolevenachi <ludwing@unicauca.edu.co>
2025-09-17 16:15:50 -0500

    09_ReduxMFGlobalState_06-swalonClear

--- 07-stores/nav/src/components/Navbar.tsx
+++ 07-stores/nav/src/components/Navbar.tsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import './navbar.css'
 import { useCounterStore } from 'storeMF/Store'
+import Swal from 'sweetalert2';
 
 const Navbar = () => {
-  const { counter, increment, decrement, clear } = useCounterStore();
+  const { counter, clear } = useCounterStore();
+
+  const handleClear = () => {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      showCancelButton: true,
+      confirmButtonText: 'Yes!',
+      cancelButtonText: 'No',
+      icon: 'warning',
+    })
+      .then((result) => {
+        if (result.isConfirmed) clear();
+      })
+
+  }
   return (
     <>
       <h1> Navbar MF {counter.value} </h1>
-      <button onClick={clear}> Clear </button>
+      <button onClick={handleClear}> Clear </button>
     </>
   )
 }
